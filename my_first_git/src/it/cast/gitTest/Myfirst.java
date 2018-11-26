0e38947964a80aac0bc62223ec99b200c60e3ac8
xiaogauishou <772620254@qq.com>
2018-11-26 11:53:45 +0800

    第一次添加

---
+++ my_first_git/src/it/cast/gitTest/Myfirst.java
@@ -0,0 +1,11 @@
+package it.cast.gitTest;
+
+public class Myfirst {
+    public static void main(String[] args) {
+        System.out.println("...............");
+        System.out.println("...............");
+        System.out.println("...............");
+        System.out.println("...............");
+        System.out.println("...............");
+    }
+}
